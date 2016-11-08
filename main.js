3ab0c9128775c7b81edc90490eb8c4744a88b48e
Brandon Semilla <grandonbroseph+gh@gmail.com>
2016-11-07 23:49:01 -0800

    Test gh-pages subtree commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function aids (s, o) {
+  return s.split('').map(x=>/[\S]/.test(x)&&(o=!o)?x.toUpperCase():x.toLowerCase()).join('')
+}
+
+var output = 'Facebook comments for the modern web'
+var overview = document.getElementById('overview')
+overview.innerText = aids(overview.innerText)
+
+new Vue({
+  el: '#wrap',
+  data: {
+    output: aids(output)
+  },
+  methods: {
+    foo: function(event) {
+      this.output = aids(event.target.value || output)
+    }
+  }
+})
