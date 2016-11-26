2215ff7defe19ab6c8a6396730d49870c8ea299c
Jamen Marz <jamenmarz+gh@gmail.com>
2016-11-25 23:54:46 -0700

    One line

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-var aids=(s,o)=>s.split('').map(x=>/[\S]/.test(x)&&(o=!o)?x.toUpperCase():x.toLowerCase()).join('')
-module.exports=aids
+module.exports=(s,o)=>s.split('').map(x=>/[\S]/.test(x)&&(o=!o)?x.toUpperCase():x.toLowerCase()).join('')
